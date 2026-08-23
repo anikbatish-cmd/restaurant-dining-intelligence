84d40db90923917588a4280ca148136f37426169
anikbatish-cmd <anik.batish@gmail.com>
2026-08-23 22:50:32 +0530

    metrics.py

--- metrics.py
+++ metrics.py
@@ -1,1 +1,19 @@
+def calculate_price_index(price, cohort_median):
+    if not price or not cohort_median:
+        return None
 
+    return price / cohort_median
+
+
+def calculate_rating_gap(rating, cohort_rating):
+    if rating is None or cohort_rating is None:
+        return None
+
+    return rating - cohort_rating
+
+
+def calculate_rating_volume_index(ratings, cohort_median):
+    if not ratings or not cohort_median:
+        return None
+
+    return ratings / cohort_median
