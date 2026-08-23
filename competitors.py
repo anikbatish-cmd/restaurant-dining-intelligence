fda17ba86b210d1b50e664a93bdb028ae8dade3f
anikbatish-cmd <anik.batish@gmail.com>
2026-08-23 22:50:53 +0530

    competitors.py

--- competitors.py
+++ competitors.py
@@ -1,1 +1,13 @@
+def rank_competitors(restaurants):
+    """
+    Placeholder for V1 competitor-ranking engine.
 
+    Future scoring will consider:
+    - distance
+    - price similarity
+    - cuisine similarity
+    - dining format
+    - occasion / positioning similarity
+    """
+
+    return restaurants
